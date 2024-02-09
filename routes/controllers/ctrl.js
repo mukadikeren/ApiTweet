59162e555936c042e9f25421d6e4ba7cf10f164d
keren <kerenhappuc4@gmail.com>
2024-02-09 10:10:08 +0200

    commit projet un

---
+++ routes/controllers/ctrl.js
@@ -0,0 +1,8 @@
+
+const getAll = (req, res) => { }
+const getOne = (req, res) => { }
+const createOne = (req, res) => { }
+const updateOne = (req, res) => { }
+const deleteOne = (req, res) => { }
+
+module.exports = { getAll, getOne, createOne,updateOne, deleteOne };
---
+++ routes/routes.js
@@ -0,0 +1,20 @@
+import {Router} from 'express'
+import {createOne, deleteOne, getAll, getOne, updateOne} from '../controllers/ctrl.js'
+import { get, post } from './controllers/ctrl.js'
+
+const router = Router()
+
+// ajouter 
+router.get('/getAll', getAll)
+// recuperer une ressource 
+router.get('/getOne', getOne)
+
+router.post('createOne', createOne)
+// mettre a jour, modifier
+router.put('updateOne/:id',updateOne)
+// supprimer
+router.delete('delete/:id', deleteOne)
+
+
+
+export default router
